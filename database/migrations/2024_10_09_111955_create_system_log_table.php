ae5e80b4c64da4df73dc376e431a8069020086e9
Mihir Patel <rs.mihirpatel19@gmail.com>
2024-10-09 17:25:57 +0530

    changes

--- database/migrations/2024_10_09_111955_create_system_log_table.php
+++ database/migrations/2024_10_09_111955_create_system_log_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('system_log', function (Blueprint $table) {
+        Schema::create('system_logs', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('inquiry_id')->default(0);
             $table->text('remark')->nullable();
